1f9b4f09ea30620e3cf5453be396ccac4cd2f7b4
shahar shvarts <shahar.shvarts@gmail.com>
2020-05-27 15:35:35 +0300

    add to sitemap

--- src/components/footer/SiteMap.js
+++ src/components/footer/SiteMap.js
@@ -1,8 +1,16 @@
 import React from "react";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import Typography from "@material-ui/core/Typography";
+
 import "./sitemap.css";
 
 function SiteMap() {
-  return <div id="site-map">מערכת דוחות מתקדמת לעסקאות</div>;
+  return (
+    <>
+      <CreditCardIcon />
+      <Typography mt={5}> מערכת דוחות מתקדמת לעסקאות </Typography>
+    </>
+  );
 }
 
 export default SiteMap;
