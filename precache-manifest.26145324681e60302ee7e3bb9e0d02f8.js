b4d0bafd0438fa6997e9ffd9c278efc07b8f2262
shahar shvarts <shahar.shvarts@gmail.com>
2020-05-31 18:47:29 +0300

    Updates

---
+++ precache-manifest.26145324681e60302ee7e3bb9e0d02f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c18bee697bd2a8ab93dbb9bc4eaca4",
+    "url": "/pos/index.html"
+  },
+  {
+    "revision": "9ba907700b59d8603aae",
+    "url": "/pos/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6f43a299309893b782f5",
+    "url": "/pos/static/css/main.7892c9ef.chunk.css"
+  },
+  {
+    "revision": "9ba907700b59d8603aae",
+    "url": "/pos/static/js/2.a86d429c.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/pos/static/js/2.a86d429c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f43a299309893b782f5",
+    "url": "/pos/static/js/main.0f00ce34.chunk.js"
+  },
+  {
+    "revision": "38cf5d9757892fc6ef91",
+    "url": "/pos/static/js/runtime-main.c57d3f12.js"
+  },
+  {
+    "revision": "2dfc9defaab40ccf2800c33a28a817a4",
+    "url": "/pos/static/media/error.2dfc9def.svg"
+  },
+  {
+    "revision": "11c02505413c45759ba77e3789024952",
+    "url": "/pos/static/media/logo.11c02505.png"
+  },
+  {
+    "revision": "451747d0b0c4192f7a5c3c2547c62405",
+    "url": "/pos/static/media/unnamed.451747d0.png"
+  }
+]);
